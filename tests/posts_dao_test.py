1f257c71d29197b27abd152e312c9b4f4ed7728d
StilllHere <106540985+StilllHere@users.noreply.github.com>
2022-12-15 16:22:50 +0300

    рабочий тест test_all_posts

--- tests/posts_dao_test.py
+++ tests/posts_dao_test.py
@@ -1,2 +1,16 @@
 from posts.PostsDAO.posts_dao import PostsDAO
 import pytest
+
+@pytest.fixture()
+def posts_dao():
+    posts_dao_instance = PostsDAO('../data/posts.json')
+    return posts_dao_instance
+
+keys_should_be = {'poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk'}
+
+class TestPostsDao:
+    def test_all_posts(self, posts_dao):
+        posts = posts_dao.get_posts_all()
+        assert type(posts) == list, "возвращается не список"
+        assert len(posts) > 0, "возвращается пустой список"
+        assert set(posts[0].keys()) == keys_should_be, "неверный список"
